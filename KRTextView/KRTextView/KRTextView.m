a5093daefaa9cfad24a62d8144fd3e8e1e263590
Konrad Rodzik <konrad.rodzik@polidea.pl>
2011-11-18 14:01:48 +0100

    changed credentials in files

--- KRTextView/KRTextView/KRTextView.m
+++ KRTextView/KRTextView/KRTextView.m
@@ -1,6 +1,8 @@
 /*
- Copyright (c) 2009, Konrad Rodzik, Polidea
+ Copyright (c) 2011, Konrad Rodzik, Polidea
  All rights reserved.
+ 
+ mailto: konrad.rodzik@gmail.com
  
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
@@ -23,7 +25,8 @@
  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+ */ 
+
 
 
 #import "KRTextView.h"
